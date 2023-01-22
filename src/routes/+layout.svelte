252141eac15b347bd6f66914be1c90cd50a3a895
fightingforchange <websitemod5@gmail.com>
2023-01-22 14:29:11 -0800

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from './Header.svelte';
+	import '@picocss/pico';
 	import './styles.css';
 </script>
 
@@ -11,7 +12,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Go to <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
 
