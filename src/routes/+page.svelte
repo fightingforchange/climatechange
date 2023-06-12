c8d72783f7d8fb02e02741899ddd0ca42168edfd
fightingforchange <websitemod5@gmail.com>
2023-06-11 23:40:09 -0700

    modified:   src/routes/+page.svelte modified:   src/routes/taking-action/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,22 @@
 	</header>
 
 <article> 
-	Climate change, an issue of grave concern, is primarily driven by the escalating levels of greenhouse gas emissions that envelop our planet. These emissions, primarily composed of carbon dioxide, methane, and other industrial gases, act like a blanket, trapping the sun's heat within the Earth's atmosphere instead of allowing it to radiate back into space. This phenomenon, known as the greenhouse effect, is the main driver of global warming, which in turn catalyzes the broader changes in climate patterns that we collectively refer to as climate change. The consequences of these changes are far-reaching and potentially catastrophic, affecting every aspect of our lives from weather patterns to food production and biodiversity. Alarmingly, the rate at which our world is warming is unprecedented in recorded history, with recent decades witnessing a rapid and consistent increase in global temperatures. This accelerated warming trend underscores the urgency of addressing climate change and the necessity of implementing sustainable practices to mitigate its impacts.
+	Climate change, a pressing global issue, is primarily driven by the increase in greenhouse gas emissions that envelop our planet. These gases, including carbon dioxide and methane, act like a blanket, trapping the sun's heat within the Earth's atmosphere, a phenomenon known as the greenhouse effect. This process results in global warming, a significant rise in the Earth's average temperature, which in turn instigates drastic changes in our climate. These changes include more frequent and severe weather events, such as hurricanes and droughts, melting polar ice caps, and rising sea levels. Alarmingly, scientific data indicates that our world is currently experiencing a rate of warming that is unprecedented in recorded history. This rapid warming underscores the urgency of addressing climate change and reducing greenhouse gas emissions to protect our planet for future generations.
+
 </article>
 
 	<details style="padding-top: 20px;">
 		<summary role="button">Why is climate change bad?</summary>
 		<ul>
-			<li>Extreme weather like droughts, huricanes, and floods are more common.</li>
-			<li>Green house gasses from fossil fuels break down the ozone layer</li>
-			<li>Ice melts causing sea level rise</li>
+			<li>Rising global temperatures: The Earth's average temperature is increasing, leading to hotter weather and heatwaves.</li>
+			<li>Melting ice caps and glaciers: Warmer temperatures are causing the polar ice caps and glaciers to melt at an accelerated rate.</li>
+			<li>Rising sea levels: The melting ice contributes to rising sea levels, which can lead to coastal flooding and the loss of land.</li>
+			<li>More frequent and severe weather events: Climate change can lead to more intense storms, hurricanes, and extreme rainfall, as well as more severe droughts and heatwaves.</li>
+			<li>Changes in precipitation patterns: Some areas may experience more rainfall while others may become drier, affecting agriculture and water availability.</li>
+			<li>Shifts in wildlife populations and habitats: Changes in temperature and precipitation can disrupt ecosystems and lead to shifts in wildlife populations and habitats. Some species may become extinct if they cannot adapt quickly enough.</li>
+			<li>Ocean acidification: Increased carbon dioxide in the atmosphere is absorbed by the oceans, leading to acidification which harms marine life, particularly shellfish and coral reefs.</li>
+			<li>Impact on human health: Climate change can lead to increased heat-related illnesses and deaths, worsened air quality, and the spread of disease-carrying insects to new areas.</li>
+			<li>Economic impacts: Damage from extreme weather events, changes in agricultural productivity, and health impacts can have significant economic costs.</li>
 		</ul>
 	</details>
 </div>
--- src/routes/taking-action/+page.svelte
+++ src/routes/taking-action/+page.svelte
@@ -13,16 +13,21 @@
 </div>
 
 <article>
-	No matter how insignificant your actions may seem, you can help fight against climate change. If
-	everyone does little things, a lot can be achieved.
+	While it may sometimes feel like individual actions are too small to make a difference in the grand scheme of climate change, it's crucial to remember that every effort counts. Each of us has a role to play in combating this global issue. Small, everyday actions, such as recycling, reducing energy consumption, or choosing public transportation over personal vehicles, can collectively have a significant impact when practiced by millions of people worldwide. If we all commit to making these seemingly minor changes, we can collectively contribute to a substantial shift towards a more sustainable future. Therefore, never underestimate the power of your actions, no matter how insignificant they may seem, because when everyone does a little, together we can achieve a lot.
 </article>
 
 <h5>Here are some things you can do:</h5>
 
 <ul>
-	<li>Walk, bike, or take public transport</li>
-	<li>Save energy at home</li>
-	<li>Raise awareness about the benefits of renewable energy</li>
+	<li>Reduce, Reuse, Recycle: Follow the three "R's" to cut down on what you throw away and conserve natural resources.</li>
+	<li>Use Energy Efficiently: Switch to energy-efficient light bulbs, unplug computers, TVs and other electronics when you're not using them.</li>
+	<li>Choose Renewable Energy: If possible, choose a utility company that generates at least half its power from wind or solar and has been certified by Green-e Energy.</li>
+	<li>Eat Less Meat: The meat industry contributes a significant amount of greenhouse gases. Even reducing meat consumption by a few meals per week can make a difference.</li>
+	<li>Drive Less and Drive Smart: Whenever possible, walk, bike, carpool, or use mass transit. Every gallon of gasoline saved keeps 20 pounds of carbon dioxide out of the atmosphere.</li>
+	<li>Plant Trees: Trees absorb carbon dioxide and are vital contributors to the environment by providing oxygen, improving air quality, conserving water, preserving soil, and supporting wildlife.</li>
+	<li>Conserve Water: Use less water to reduce the energy required to process and deliver it to homes, businesses, farms, and communities, which helps reduce pollution and conserve fuel resources.</li>
+	<li>Educate Others: Share information about climate change with friends, relatives, colleagues. The more people are aware, the more they can contribute to the solution.</li>
+	<li>Vote and Advocate: Support policies and candidates at the local, state, and national levels who take strong action on climate change.</li>
 </ul>
 
 <style>
